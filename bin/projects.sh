93f588075732b632752ca104989a040a3c9827c3
Kazuto Kusama <jacopen@gmail.com>
2019-10-21 13:13:27 +0900

    Add scripts

---
+++ bin/projects.sh
@@ -0,0 +1,2 @@
+curl -i -XPOST -H"Content-Type: application/json" localhost:8083/projects -d"{\"name\": \"Basket Weaving\", \"accountId\": ${ACCOUNT_ID}}"
+curl -i localhost:8083/projects?accountId=${ACCOUNT_ID}
---
+++ bin/stories.sh
@@ -0,0 +1,2 @@
+curl -i -XPOST -H"Content-Type: application/json" localhost:8082/stories -d"{\"projectId\": ${PROJECT_ID}, \"name\": \"Find some reeds\"}"
+curl -i localhost:8082/stories?projectId=${PROJECT_ID}
